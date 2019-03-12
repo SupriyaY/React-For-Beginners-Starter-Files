dbdf50020fe5ca47d0e0b4e2a76e1a4794b83640
Supriya Yerramilli <supriyayerramilli@Supriyas-MacBook-Pro.local>
2019-03-12 00:25:34 -0400

    rendering fish1 through fish9 on page

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Header from './Header';
 import Order from './Order';
-import Inventory from './Inventory';
+import Inventory from './Inventory'; 
 import sampleFishes from '../sample-fishes';
+import Fish from './Fish';
 
 class App extends React.Component {
   state = {
@@ -28,9 +29,13 @@ loadSamplesFishes = () => {
     <div className="catch-of-the-day">
     <div className="menu">
     <Header tagline="Fresh Seafood Market"/>
+    <ul className="fishes">
+    {Object.keys(this.state.fishes).map(key => <p>{key}</p>)}
+    </ul>
     </div>
      <Order/>
-    <Inventory addFish={this.addFish} loadSampleFishes={this.loadSamplesFishes}/>
+    <Inventory addFish={this.addFish} 
+    loadSampleFishes={this.loadSamplesFishes}/>
     </div>
     );
   }
---
+++ catch-of-the-day/src/components/Fish.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Fish extends React.Component {
+    render () {
+        return (
+            <div className="single-fish">
+                <p>fish</p>
+            </div>
+        )
+    }
+}
+
+export default Fish;
